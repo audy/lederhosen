cdab7c96bac91979fa53d6db9fe7870b2dc19d13
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-07-16 16:32:10 -0400

    cleaned up whitespace

--- lib/lederhosen/tasks/otu_table.rb
+++ lib/lederhosen/tasks/otu_table.rb
@@ -17,32 +17,32 @@ module Lederhosen
       input        = options[:clusters]
       output       = options[:output]
       joined_reads = options[:joined]
-      
+
       # Load cluster table
 
-      clstr_info      = Helpers.load_uc_file input     
+      clstr_info      = Helpers.load_uc_file input
       clstr_counts    = clstr_info[:clstr_counts] # clstr_counts[:clstr][sample.to_i] = reads
       clstrnr_to_seed = clstr_info[:clstrnr_to_seed]
       samples         = clstr_info[:samples]
 
       # print OTU abundance matrix
-  		# clusters as columns
-  		# samples as rows
+      # clusters as columns
+      # samples as rows
 
       File.open("#{output}.csv", 'w') do |h|
         samples  = samples.sort
         clusters = clstr_counts.keys
 
         # print header (cluster names)
-  			h.puts '-' + SEP + clusters.map { |x| "cluster-#{x}" }.join(SEP)
+        h.puts '-' + SEP + clusters.map { |x| "cluster-#{x}" }.join(SEP)
 
-  			samples.each do |sample|
-  				h.print sample
-  				clusters.each do |cluster|
-  					h.print "#{SEP}#{clstr_counts[cluster][sample]}"
-  				end
-  				h.print "\n"
-  			end
+        samples.each do |sample|
+          h.print sample
+          clusters.each do |cluster|
+            h.print "#{SEP}#{clstr_counts[cluster][sample]}"
+          end
+          h.print "\n"
+        end
       end
     end
 
