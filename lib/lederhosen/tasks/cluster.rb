0576483af241257a22bf2394c0c4e4f1d042f4f8
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-07-26 17:07:02 -0400

    new options for uclust

--- lib/lederhosen/tasks/cluster.rb
+++ lib/lederhosen/tasks/cluster.rb
@@ -8,20 +8,32 @@ module Lederhosen
     desc "cluster",
          "cluster a fasta file using UCLUST"
 
-    method_option :input,    :type => :string,  :required => true
-    method_option :output,   :type => :string,  :required => true
-    method_option :identity, :type => :numeric, :required => true
+    method_option :input,       :type => :string,  :required => true
+    method_option :output,      :type => :string,  :required => true
+    method_option :identity,    :type => :numeric, :required => true
+    method_option :stepwords,   :type => :numeric, :default  => 8
+    method_option :wordlen,    :type  => :numeric, :default  => 8
+    method_option :maxaccepts, :type  => :numeric, :default  => 1
+    method_option :maxrejects, :type  => :numeric, :default  => 8
 
     def cluster
-      identity = options[:identity]
-      output = options[:output]
-      input = options[:input]
+      identity   = options[:identity]
+      output     = options[:output]
+      input      = options[:input]
+      stepwords  = options[:stepwords]
+      maxaccepts = options[:maxaccepts]
+      maxrejects = options[:maxrejects]
+      wordlen    = options[:wordlen]
 
       cmd = [
         'uclust',
         "--input #{input}",
         "--uc #{output}",
         "--id #{identity}",
+        "--stepwords #{stepwords}",
+        "--maxaccepts #{maxaccepts}",
+        "--maxrejects #{maxrejects}",
+        "--w #{wordlen}"
       ].join(' ')
 
       @shell.mute { run cmd }
