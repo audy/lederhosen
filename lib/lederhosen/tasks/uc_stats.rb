d13f94e948d0c8561e02ba5843c94ffd6d32a6b9
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-07-26 16:14:56 -0400

    added uc_stats to get statistics about clusters (so far only size distribution)

---
+++ lib/lederhosen/tasks/uc_stats.rb
@@ -0,0 +1,39 @@
+##
+# Get statistics about clusters in a UC file
+#
+
+module Lederhosen
+  class CLI
+    desc 'uc_stats',
+      'get statistics about clusters in a UC file. for now, this only calculates the size of each cluster'
+
+    method_option :input, :type => :string, :required => true
+
+    def uc_stats
+      input = options[:input]
+
+      # TODO add more stats
+      cluster_stats = Hash.new { |h, k|
+        h[k] = {
+          :size  => 0
+        }
+      }
+
+      File.open(input) do |handle|
+        handle.each do |line|
+          line = line.strip.split
+          type, clustr_nr = line[0], line[1]
+          cluster_stats[clustr_nr][:size] += 1
+        end
+      end
+
+      stat_types = cluster_stats.values.first.keys.sort
+
+      puts "cluster,#{stat_types.join(',')}"
+      cluster_stats.each do |cluster, stats|
+        puts "#{cluster},#{stat_types.map { |x| stats[x] }.join(',')}"
+      end
+    end
+
+  end
+end
