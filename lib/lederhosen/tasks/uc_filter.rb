59b4c18c2263dc86c763d27c4375a746daad064b
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-08-08 17:21:34 -0400

    more statuses

--- lib/lederhosen/tasks/uc_filter.rb
+++ lib/lederhosen/tasks/uc_filter.rb
@@ -23,10 +23,12 @@ module Lederhosen
       ohai "filtering #{input} to #{output}, reads = #{reads} & samples = #{samples}"
 
       # load UC file
+      ohai "loading uc file"
       clstr_info   = Helpers.load_uc_file input
       clstr_counts = clstr_info[:clstr_counts] # clstr_counts[:clstr][sample.to_i] = reads
 
       # filter
+      ohai "filtering"
       survivors = clstr_counts.reject do |a, b|
         b.reject{ |i, j| j < reads }.length < samples
       end
@@ -34,13 +36,18 @@ module Lederhosen
       surviving_clusters = survivors.keys.to_set
 
       # print filtered uc file
+      ohai "saving filtered table"
       out = File.open(output, 'w')
-      kept, total = 1, 0
+
       lines = `wc -l #{input}`.split.first.to_i
+
       pbar = ProgressBar.new 'saving', lines
+      kept, total = 1, 0
+
       File.open(input) do |handle|
         handle.each do |line|
           pbar.inc
+
           if line =~ /^#/
             out.print line
             next
@@ -48,15 +55,18 @@ module Lederhosen
 
           total += 1
 
+          # check if cluster is in surviving clusters
           if surviving_clusters.include? line.split[1].to_i
             out.print line
             kept += 1
           end
+
         end
       end
+
       pbar.finish
       out.close
-      ohai "Survivors"
+
       ohai "clusters: #{surviving_clusters.length}/#{clstr_counts.keys.length} = #{100*surviving_clusters.length/clstr_counts.keys.length.to_f}%"
       ohai "reads:    #{kept}/#{total} = #{100*kept/total.to_f}%"
     end
