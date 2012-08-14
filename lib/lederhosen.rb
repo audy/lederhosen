c359ee79aa630159a6f1c4a913ba2c591499ef4a
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-08-13 20:31:19 -0400

    code comments

--- lib/lederhosen.rb
+++ lib/lederhosen.rb
@@ -7,6 +7,11 @@ require 'set'
 
 Dir.glob(File.join(File.dirname(__FILE__), 'lederhosen', '*.rb')).each { |f| require f }
 
+##
+# Here I extend the string class to add the to_kmers method
+#
+# Does it really need to be a method of String? Sure, why not?
+#
 class String
   def to_kmers(k)
     return [] if k == 0
