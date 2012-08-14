7b04a3b3595d6ce6aecb231a3f2df4556e3b2f70
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-08-14 13:57:34 -0400

    otu filter was originally made for transposed tables. now it is fixed

--- lib/lederhosen/tasks/otu_filter.rb
+++ lib/lederhosen/tasks/otu_filter.rb
@@ -1,55 +1,46 @@
+require 'set'
+
 module Lederhosen
   class CLI
 
     desc 'otu_filter', 'works like uc_filter but uses an OTU table as input'
 
-    method_option :input, :type   =>  :string, :required => true
-    method_option :output, :type  =>  :string, :required => true
-    method_option :reads, :type   => :numeric, :required => true
+    method_option :input,   :type => :string,  :required => true
+    method_option :output,  :type => :string,  :required => true
+    method_option :reads,   :type => :numeric, :required => true
     method_option :samples, :type => :numeric, :required => true
 
     def otu_filter
-      input   = options[:input]
-      output  = options[:output]
-      reads   = options[:reads]
+      input       = options[:input]
+      output      = options[:output]
+      reads       = options[:reads]
       min_samples = options[:samples]
 
       ohai "filtering otu file #{input} (reads = #{reads}, samples = #{min_samples}), saving to #{output}"
 
-      ##
-      # Iterate over otu table line by line.
-      # Only print if cluster meets criteria
-      #
-      kept_clusters = 0
-      total_reads   = 0
-      kept_reads    = 0
+      cluster_sample_count = Hash.new { |h, k| h[k] = Hash.new }
 
-      out = File.open(output, 'w')
-
-      File.open(input) do |handle|
-        header  = handle.gets.strip
-        header  = header.split(',')
-        samples = header[1..-1]
-
-        out.puts header.join(',')
-
+      # slurp up CSV file
+      File.open input do |handle|
+        header = handle.gets.strip.split(',')
+        cluster_ids = header[1..-1]
         handle.each do |line|
-          line       = line.strip.split(',')
-          cluster_no = line[0]
-          counts     = line[1..-1].collect { |x| x.to_i }
-
-          # should be the same as uc_filter
-          if counts.reject { |x| x < reads }.length > min_samples
-            out.puts line.join(',')
-            kept_clusters += 1
-            kept_reads += counts.inject(:+)
+          line = line.strip.split(',')
+          sample_id = line[0].to_sym
+          counts = line[1..-1].map(&:to_i)
+          cluster_ids.zip(counts).each do |cluster, count|
+            cluster_sample_count[cluster][sample_id] = count
           end
-          total_reads += counts.inject(:+)
         end
       end
 
-      ohai "kept #{kept_reads} reads (#{kept_reads/total_reads.to_f})."
-      ohai "kept #{kept_clusters} clusters."
+      # filter sample_cluster_count
+      filtered = cluster_sample_count.reject { |k, v| v.reject { |k, v| v < reads }.size < min_samples }
+
+      ohai "kept #{filtered.keys.size} clusters (#{filtered.keys.size/cluster_sample_count.size.to_f})."
+      kept_reads = filtered.values.map { |x| x.values.inject(:+) }.inject(:+)
+      total_reads = cluster_sample_count.values.map { |x| x.values.inject(:+) }.inject(:+)
+      ohai "kept #{kept_reads}/#{total_reads} reads (#{kept_reads/total_reads.to_f})."
     end
 
   end
