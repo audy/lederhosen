d53a86ae84e211564eeedfd59dee969900355a0f
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-08-27 17:02:43 -0400

    added option to join unpaired reads

--- lib/lederhosen/tasks/join.rb
+++ lib/lederhosen/tasks/join.rb
@@ -5,14 +5,16 @@ module Lederhosen
     # PAIRED-END READ WORK-AROUND (JOIN THEM)
     #
     desc "join",
-         "join trimmed reads into a single file"
+      "join paired or unpaired reads into a single file. Paired reads are joined end-to-end"
 
     method_option :trimmed, :type => :string, :required => true
     method_option :output,  :type => :string, :required => true
+    method_option :paired,  :type => :boolean, :default => true
 
     def join
       trimmed = Dir[options[:trimmed]]
       output  = options[:output]
+      paired  = options[:paired]
 
       ohai "joining #{File.dirname(trimmed.first)} saving to #{output}"
 
@@ -24,20 +26,43 @@ module Lederhosen
 
       trimmed.each do |fasta_file|
         pbar.inc
+        records =
+          begin
+            Dna.new File.open(fasta_file)
+          rescue
+            ohai "skipping #{fasta_file} (empty?)"
+            next
+          end
 
-        begin
-          records = Dna.new File.open(fasta_file)
-        rescue
-          ohai "skipping #{fasta_file} (empty?)"
-          next
-        end
-
-        records.each_slice(2) do |l, r|
-          output.puts ">#{r.name}:split=#{r.sequence.size}:sample=#{File.basename(fasta_file, '.fasta')}\n#{r.sequence.reverse+l.sequence}"
+        if paired
+          output_paired_reads(records, output, fasta_file)
+        else
+          output_unpaired_reads(records, output, fasta_file)
         end
       end
       pbar.finish
     end
 
+    no_tasks do
+      ##
+      # Output paired reads joined together
+      #
+      def output_paired_reads(records, output, fasta_file)
+        records.each_slice(2) do |l, r|
+          output.puts ">#{r.name}:split=#{r.sequence.size}:sample=#{File.basename(fasta_file, '.fasta')}"
+          output.puts "#{r.sequence.reverse+l.sequence}"
+        end
+      end
+
+      ##
+      # Output unpaired reads
+      #
+      def output_unpaired_reads(records, output, fasta_file)
+        records.each do |r|
+          output.puts ">#{r.name}:split=na:sample=#{File.basename(fasta_file, '.fasta')}"
+          output.puts r.sequence
+        end
+      end
+    end
   end
 end
