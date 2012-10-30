efee1559faed07a7062414fffdc0da289c11b4ca
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-10-30 17:03:39 -0400

    1.1.1

--- lib/lederhosen/version.rb
+++ lib/lederhosen/version.rb
@@ -3,7 +3,7 @@ module Lederhosen
     MAJOR = 1
     MINOR = 1
     CODENAME = 'Apfelstrudel' # changes for minor versions
-    PATCH = 0
+    PATCH = 1
 
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end
