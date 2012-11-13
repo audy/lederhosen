27e9ad0460da849bb0ad16cd4616ca61bad06f9b
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-11-13 14:35:37 -0500

    version bump!

--- lib/lederhosen/version.rb
+++ lib/lederhosen/version.rb
@@ -3,7 +3,7 @@ module Lederhosen
     MAJOR = 1
     MINOR = 2
     CODENAME = 'Regenmantel' # changes for minor versions
-    PATCH = 0
+    PATCH = 1
 
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end
