d2332f3fe0f21d6a4c5c0d96dd4b357f966466bf
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-12-04 09:46:16 -0500

    version bump

--- lib/lederhosen/version.rb
+++ lib/lederhosen/version.rb
@@ -3,7 +3,7 @@ module Lederhosen
     MAJOR = 1
     MINOR = 3
     CODENAME = 'Dirndl' # changes for minor versions
-    PATCH = 4
+    PATCH = 5
 
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end
