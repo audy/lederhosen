25a186c8e1b0e864d98989e33ee18e987150df32
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-12-13 17:36:12 -0500

    added placeholder for trimming spec. need to verify that lengths are acceptable

--- spec/trimmer_spec.rb
+++ spec/trimmer_spec.rb
@@ -2,25 +2,35 @@ require 'spec_helper'
 
 describe Lederhosen::Trimmer do
 
-  describe Lederhosen::Trimmer::PairedTrimmer do
-
-  end
+  describe Lederhosen::Trimmer::PairedTrimmer
 
   describe Lederhosen::Trimmer::SequenceTrimmer do
-    let(:sequence_trimmer) { Lederhosen::Trimmer::SequenceTrimmer.new(:cutoff => 64, :min => 20) }
 
-    it 'can trim a record' do
+    let :sequence_trimmer do
+      Lederhosen::Trimmer::SequenceTrimmer.new(:offset => 64, :min => 20)
     end
 
     it 'trims records as expected' do
-      # now we need some examples
+
+      trimmed_sizes = File.readlines('spec/data/trimmed_sizes.txt').map &:to_i
+
+      File.open('spec/data/example.fastq') do |handle|
+        records = Dna.new handle
+        records.each do |record|
+          trimmed_record = sequence_trimmer.trim_seq record
+          trimmed_record.size.should == trimmed_sizes.shift
+        end
+      end
     end
 
   end
 
   describe Lederhosen::Trimmer::QSEQTrimmer do
 
-    let(:qseq_trimmer) { Lederhosen::Trimmer::QSEQTrimmer.new 'spec/data/ILT_L_9_B_001_1.txt.gz', 'spec/data/ILT_L_9_B_001_3.txt.gz' }
+    let :qseq_trimmer do
+      Lederhosen::Trimmer::QSEQTrimmer.new 'spec/data/ILT_L_9_B_001_1.txt.gz',
+                                           'spec/data/ILT_L_9_B_001_3.txt.gz'
+    end
 
     it 'can be initialized' do
       qseq_trimmer.should_not be_nil
@@ -39,7 +49,9 @@ describe Lederhosen::Trimmer do
 
   describe Lederhosen::Trimmer::InterleavedTrimmer do
 
-    let(:interleaved_trimmer) { Lederhosen::Trimmer::InterleavedTrimmer.new 'spec/data/example.fastq' }
+    let :interleaved_trimmer do
+      Lederhosen::Trimmer::InterleavedTrimmer.new 'spec/data/example.fastq'
+    end
 
     it 'can be initialized' do
       interleaved_trimmer.should_not be_nil
