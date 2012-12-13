c22dc2554576ea5eb21ef533e1e7f3787ec061d7
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-12-13 14:50:13 -0500

    major version bump

--- lib/lederhosen/version.rb
+++ lib/lederhosen/version.rb
@@ -1,8 +1,8 @@
 module Lederhosen
   module Version
     MAJOR = 1
-    MINOR = 4
-    CODENAME = 'Strickjacke' # changes for minor versions
+    MINOR = 6
+    CODENAME = 'Sauerkraut' # changes for minor versions
     PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].join('.')
