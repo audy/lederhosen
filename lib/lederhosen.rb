36e35ed4a634ce3f1edaaa62926eaedbe640c216
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-12-13 14:47:28 -0500

    no longer need String#to_kmers

--- lib/lederhosen.rb
+++ lib/lederhosen.rb
@@ -6,16 +6,3 @@ require 'progressbar'
 require 'thor'
 
 Dir.glob(File.join(File.dirname(__FILE__), 'lederhosen', '*.rb')).each { |f| require f }
-
-##
-# Here I extend the string class to add the to_kmers method
-#
-# Does it really need to be a method of String? Sure, why not?
-#
-class String
-  def to_kmers(k)
-    return [] if k == 0
-    k -= 1
-    (0..(self.length-k-1)).collect { |i| self[i..i+k] }
-  end
-end
