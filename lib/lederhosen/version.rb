3c9b67243e8b5f032f588c8540cbdde70544a4ca
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-12-13 15:32:38 -0500

    version bump

--- lib/lederhosen/version.rb
+++ lib/lederhosen/version.rb
@@ -3,7 +3,7 @@ module Lederhosen
     MAJOR = 1
     MINOR = 6
     CODENAME = 'Sauerkraut' # changes for minor versions
-    PATCH = 0
+    PATCH = 1
 
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end
