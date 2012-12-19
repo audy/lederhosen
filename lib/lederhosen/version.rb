64d057722823cadb9cde0a0230ad28f5c188d90f
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-12-19 14:45:37 -0500

    new version!

--- lib/lederhosen/version.rb
+++ lib/lederhosen/version.rb
@@ -1,9 +1,9 @@
 module Lederhosen
   module Version
     MAJOR = 1
-    MINOR = 6
-    CODENAME = 'Sauerkraut' # changes for minor versions
-    PATCH = 2
+    MINOR = 7
+    CODENAME = 'Franziskaner' # changes for minor versions
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end
