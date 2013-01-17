4a9fd052204ee4d1a0830f16a8c3a61b7e2e3b5e
audy <harekrishna@gmail.com>
2013-01-17 17:20:16 -0500

    no longer need trimming speces, use test .fasta file for clustering specs

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -12,30 +12,20 @@ describe Lederhosen::CLI do
     $?.success?.should be_true
   end
 
-  it 'should trim paired QSEQ reads' do
-    `./bin/lederhosen trim --reads-dir=spec/data/IL*.txt.gz --out-dir=#{$test_dir}/trimmed`
-    $?.success?.should be_true
-  end
-
-  it 'should trim paired, interleaved FASTQ reads' do
-    `./bin/lederhosen trim --reads-dir=spec/data/example.fastq --out-dir=#{$test_dir}/trimmed --read-type fastq`
-    $?.success?.should be_true
-  end
-
   it 'can create a usearch udb using usearch' do
-    `./bin/lederhosen make_udb --input #{$test_dir}/trimmed/ILT_L_9_B_001.fasta --output #{$test_dir}/test_db.udb`
+    `./bin/lederhosen make_udb --input spec/data/trimmed/ILT_L_9_B_001.fasta --output #{$test_dir}/test_db.udb`
     $?.success?.should be_true
   end
 
   it 'can (dry run) cluster reads using usearch and output usearch cmd to stdout' do
-    stdout = `./bin/lederhosen cluster --dry-run --input #{$test_dir}/trimmed/ILT_L_9_B_001.fasta --database #{$test_dir}/test_db.udb --identity 0.95 --output #{$test_dir}/clusters.uc`
+    stdout = `./bin/lederhosen cluster --dry-run --input spec/data/trimmed/ILT_L_9_B_001.fasta --database #{$test_dir}/test_db.udb --identity 0.95 --output #{$test_dir}/clusters.uc`
     stdout.should match /^usearch/
     $?.success?.should be_true
     File.exists?(File.join($test_dir, 'clusters.uc')).should be_false
   end
 
   it 'can cluster reads using usearch' do
-    `./bin/lederhosen cluster --input #{$test_dir}/trimmed/ILT_L_9_B_001.fasta --database #{$test_dir}/test_db.udb --identity 0.95 --output #{$test_dir}/clusters.uc`
+    `./bin/lederhosen cluster --input spec/data/trimmed/ILT_L_9_B_001.fasta --database #{$test_dir}/test_db.udb --identity 0.95 --output #{$test_dir}/clusters.uc`
     $?.success?.should be_true
     File.exists?(File.join($test_dir, 'clusters.uc')).should be_true
   end
@@ -57,12 +47,12 @@ describe Lederhosen::CLI do
   end
 
   it 'should split a fasta file into smaller fasta files (optionally gzipped)' do
-    `./bin/lederhosen split_fasta --input=#{$test_dir}/trimmed/ILT_L_9_B_001.fasta --out-dir=#{$test_dir}/split/ --gzip true -n 100`
+    `./bin/lederhosen split_fasta --input=spec/data/trimmed/ILT_L_9_B_001.fasta --out-dir=#{$test_dir}/split/ --gzip true -n 100`
     $?.success?.should be_true
   end
 
   it 'should print representative sequences from uc files' do
-    `./bin/lederhosen get_reps --input=#{$test_dir}/clusters.uc --database=#{$test_dir}/trimmed/ILT_L_9_B_001.fasta --output=#{$test_dir}/representatives.fasta`
+    `./bin/lederhosen get_reps --input=#{$test_dir}/clusters.uc --database=spec/data/trimmed/ILT_L_9_B_001.fasta --output=#{$test_dir}/representatives.fasta`
   end
 
   it 'should create a fasta file containing representative reads for each cluster'
