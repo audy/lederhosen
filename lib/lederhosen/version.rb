7d1a3198283f50f473aeecaa516c7424ad98aa68
audy <harekrishna@gmail.com>
2013-01-23 16:50:25 -0500

    version bump

--- lib/lederhosen/version.rb
+++ lib/lederhosen/version.rb
@@ -1,9 +1,9 @@
 module Lederhosen
   module Version
-    MAJOR = 1
-    MINOR = 8
-    CODENAME = 'Karottensaft' # changes for minor versions
-    PATCH = 2
+    MAJOR = 2
+    MINOR = 0
+    CODENAME = 'Schnittlauchbrot' # changes for minor versions
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end
