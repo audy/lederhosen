48a17a3529c5b04c3eb5f56f6cc8f3db91f1a301
audy <harekrishna@gmail.com>
2013-06-12 11:40:41 -0400

    no more pre string

--- lib/lederhosen/version.rb
+++ lib/lederhosen/version.rb
@@ -4,13 +4,9 @@ module Lederhosen
     MINOR = 0
     CODENAME = 'Biergarten' # changes for minor versions
     PATCH = 0
-    PRE = 'dev'
 
     string = [MAJOR, MINOR, PATCH].join('.')
-    if PRE
-      string = string + "-#{PRE}"
-    end
-    
+
     STRING = string
   end
 end
