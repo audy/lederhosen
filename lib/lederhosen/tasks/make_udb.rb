d49ffcb7fb62e7040a32ed87bdc1abc36aee4c4b
audy <harekrishna@gmail.com>
2013-06-12 11:42:56 -0400

    wordlength and dbstep failing, removed. Check with Robert Edgar

--- lib/lederhosen/tasks/make_udb.rb
+++ lib/lederhosen/tasks/make_udb.rb
@@ -5,8 +5,6 @@ module Lederhosen
 
     method_option :input,  :type => :string, :required => true
     method_option :output, :type => :string, :required => true
-    method_option :word_length, :type => :numeric, :default => 64
-    method_option :db_step, :type => :numeric, :default => 4
 
     def make_udb
       input       = options[:input]
@@ -19,8 +17,6 @@ module Lederhosen
       cmd = ['usearch',
              "-makeudb_usearch #{input}",
              "-output #{output}",
-             "-wordlength #{word_length}",
-             "-dbstep #{db_step}",
             ]
 
       cmd = cmd.join(' ')
